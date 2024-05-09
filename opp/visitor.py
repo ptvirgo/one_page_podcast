fa254f59c90ef80c09999900c4f4029db0a81fcb
Pablo Virgo <mailbox@pablovirgo.com>
2024-05-09 16:17:45 -0400

    Visitor interface doc / timing tweak.

--- opp/visitor.py
+++ opp/visitor.py
@@ -15,49 +15,13 @@ class PodcastDatastore(ABC):
 
     @abstractmethod
     def get_channel(self):
-        """
-        Produce a dict describing the podcast channel, as follows:
-
-            {
-                title: str
-                link: str, url
-                description: str
-                image: image url
-                author: str
-                email: str, email address
-                language: "en" or similar language code
-                category: str - Apple podcast defines a limited set
-                explicit: boolean
-                keywords: list of strings
-            }
-
-        """
+        """Produce the podcast channel."""
         pass
 
 
     @abstractmethod
     def get_episodes(self):
-        """
-        Produce an iterable of podcast episode descriptors, as follows:
-
-            [
-                {
-                    title: str
-                    link: str, url
-                    description: str
-                    guid: str, globally unique identifier
-                    duration: int, seconds
-
-                    pubDate: iso formatted date string
-                    image: str, url
-               
-                    file_name: str url
-                    audio_format: str, "mp3", "ogg", "opus"
-                    length: int, bytes
-                }
-            ]
-
-        """
+        """Produce an iterable of podcast episodes."""
         pass
 
 
@@ -65,25 +29,26 @@ class VisitPodcast:
 
     def __init__(self, loader):
         self.loader = loader
-        self.channel = self.loader.get_channel()
-        self.episodes = self.loader.get_episodes()
 
     def podcast_data(self):
         """Produce a dict of all fields needed to follow the podcast."""
 
+        channel = self.loader.get_channel()
+        episodes = self.loader.get_episodes()
+
         return \
             { "channel":
                 {
-                    "title": self.channel.title,
-                    "link": self.channel.link,
-                    "description": self.channel.description,
-                    "image": self.channel.image,
-                    "author": self.channel.author,
-                    "email": self.channel.email,
-                    "language": self.channel.language,
-                    "category": self.channel.category,
-                    "explicit": self.channel.explicit,
-                    "keywords": self.channel.keywords
+                    "title": channel.title,
+                    "link": channel.link,
+                    "description": channel.description,
+                    "image": channel.image,
+                    "author": channel.author,
+                    "email": channel.email,
+                    "language": channel.language,
+                    "category": channel.category,
+                    "explicit": channel.explicit,
+                    "keywords": channel.keywords
                 },
-                "episodes": [ dict(ep) for ep in self.episodes ]
+                "episodes": [ dict(ep) for ep in episodes ]
             }
